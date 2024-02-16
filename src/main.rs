ff29bc44cd0688e7d413057db42cbf1d8826b591
bradford lamson-scribner <brad.lamson@gmail.com>
2024-02-16 15:27:48 -0700

    refactor: update bg main to take CLI args for path to file

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,22 @@
 use bg::ByteGazer;
+use std::{env, process};
 
 mod bg;
 mod colors;
 
 fn main() {
-    // let path = "test-hex-program";
-    let path = "test.hex";
-    let byte_gazer = match ByteGazer::new(path) {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 2 {
+        eprintln!("usage: bytegazer <path_to_file>");
+        process::exit(1);
+    }
+
+    let file_path = &args[1];
+    let byte_gazer = match ByteGazer::new(file_path) {
         Ok(bg) => bg,
         Err(e) => {
-            eprintln!("Failed to create ByteGazer: {}", e);
-            return;
+            eprintln!("failed to initialize ByteGazer: {}", e);
+            process::exit(1);
         }
     };
 
